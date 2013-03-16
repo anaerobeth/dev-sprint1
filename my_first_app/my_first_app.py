286a28d79f1229f6fff8091927efe2d9012e50b2
Elizabeth Tenorio <betsky98@gmail.com>
2013-03-16 00:26:53 -0400

    add index.html

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -1,2 +1,27 @@
 # This is whre you can start you python file for your week1 web app
-# Name: 
+# Name: Elizabeth Tenorio
+
+import flask, flask.views
+import os
+
+app = flask.Flask(__name__)
+
+app.secret_key = "dan"
+
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+
+    def post(self):
+        s = str(flask.request.form['expression'])
+        r = ""
+        for i in s:
+            r = i + r
+        flask.flash(r)
+        return self.get()
+
+
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+
+app.debug = True
+app.run()
