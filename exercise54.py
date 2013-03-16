c0a72a84800de4e4b0462a4b1d8c89b09785f345
Elizabeth Tenorio <betsky98@gmail.com>
2013-03-16 01:53:55 -0400

    Add exercise54.py and koch_curve.png to swampy

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,28 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Elizabeth Tenorio
+
+import math
+from TurtleWorld import *
+world = TurtleWorld()
+bob = Turtle()
+
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    fd(t, length*n)
+    lt(t, angle)
+    draw(t, length, n-1)
+    rt(t, 2*angle)
+    draw(t, length, n-1)
+    lt(t, angle)
+    bk(t, length*n)
+
+# This is the code to construct a Koch curve
+#def koch(turtle, len):
+#    for i in range(4):
+#        fd(bob, 100)
+#        lt(bob)
+
+draw(bob, 4, 50)
