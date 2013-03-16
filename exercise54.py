6e5aaa0d97dc06f20018a0d28ee9f16e38be8828
Elizabeth Tenorio <betsky98@gmail.com>
2013-03-16 01:58:06 -0400

    Add exercise54.py

--- exercise54.py
+++ exercise54.py
@@ -1,28 +1,63 @@
 # This is where Exercise 5.4 goes
 # Name: Elizabeth Tenorio
 
-import math
 from TurtleWorld import *
 world = TurtleWorld()
 bob = Turtle()
 
-
-def draw(t, length, n):
-    if n == 0:
-        return
-    angle = 50
-    fd(t, length*n)
-    lt(t, angle)
-    draw(t, length, n-1)
-    rt(t, 2*angle)
-    draw(t, length, n-1)
-    lt(t, angle)
-    bk(t, length*n)
-
 # This is the code to construct a Koch curve
-#def koch(turtle, len):
-#    for i in range(4):
-#        fd(bob, 100)
-#        lt(bob)
+# x = length
+def koch(t, x):
+  if x < 3:
+    fd(t, x)
+    return
+  else:
+    koch(t, x/3)
+    lt(t, 60)
+    koch(t, x/3)
+    rt(t, 120)
+    koch(t, x/3)
+    lt(t, 60)
+    koch(t, x/3)
 
-draw(bob, 4, 50)
+#koch(bob, 500)
+
+#Make a snowflake with n koch curves
+def snowflake(t, x, n):
+  if n <=0:
+    return
+  else:
+    koch(t, x)
+    n = n - 1
+    snowflake(t, x, n)
+#snowflake(bob, 500, 3)
+
+#Cesaro fractal
+# x = length
+def cf(t, x):
+  if x < 3:
+    fd(t, x)
+    return
+  else:
+    cf(t, x/3)
+    lt(t, 85)
+    cf(t, x/3)
+    rt(t, 170)
+    cf(t, x/3)
+    lt(t, 85)
+    cf(t, x/3)
+
+#cf(bob, 500)
+
+#Make a Cesaro snowflake
+def csnowflake(t, x, n):
+  if n <=0:
+    return
+  else:
+    cf(t, x)
+    n = n - 1
+    csnowflake(t, x, n)
+#csnowflake(bob, 100, 3)
+
+wait_for_user()
+
